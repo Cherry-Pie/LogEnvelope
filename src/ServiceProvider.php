f624a9b16b865efc7cb86c250922c90a96836b7a
Yaro <12fcv4@gmail.com>
2015-08-03 06:44:52 -0400

    package structure enh

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -11,7 +11,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/config/log-envelope.php' => config_path('yaro.log-envelope.php'),
+            __DIR__.'/../config/log-envelope.php' => config_path('yaro.log-envelope.php'),
         ]);
         
         $this->app['view']->addNamespace('log-envelope', __DIR__ . '/../resources/views');
