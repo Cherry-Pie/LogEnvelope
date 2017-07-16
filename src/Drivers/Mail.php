b90f2e6b8e114974d2d3160f69d9f9638e4082a8
Yaro <12fcv4@gmail.com>
2017-07-16 15:57:36 +0300

    support for lower illuminate version than 5.3

--- src/Drivers/Mail.php
+++ src/Drivers/Mail.php
@@ -28,7 +28,32 @@ class Mail extends AbstractDriver
         $data = $this->data;
         $config = $this->config;
         
-        MailFacade::queue(new LogEmail($data, $config));
+        if ($this->isMailablePossible()) {
+            MailFacade::queue(new LogEmail($data, $config));
+            return;
+        }
+        
+        MailFacade::queue('log-envelope::main', $data, function($message) use ($data, $config) {
+            $subject = sprintf('[%s] @ %s: %s', $data['class'], $data['host'], $data['exception']);
+            
+             // to protect from gmail's anchors automatic generation
+            $message->setBody(
+                preg_replace(
+                    ['~\.~', '~http~'],
+                    ['<span>.</span>', '<span>http</span>'],
+                    $message->getBody()
+                )
+            );
+            
+             $message->to($config['to'])
+                     ->from($config['from_email'], $config['from_name'])
+                     ->subject($subject);
+        });
     } // end send
+    
+    private function isMailablePossible()
+    {
+        return class_exists('Illuminate\Mail\Mailable');
+    } // end isMailablePossible
     
 }
